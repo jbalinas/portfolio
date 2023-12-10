247a43c039bc487d413da6bdea9d6ff7ffedcf38
jbalinas <108829766+jbalinas@users.noreply.github.com>
2023-12-10 00:24:39 -0800

    File layout complete, created basic structure for html, as well as styling and interactivity.

---
+++ src/script.js
@@ -0,0 +1,14 @@
+// this is where our interactivity goes
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    const navItems = document.querySelectorAll('header nav ul li a');
+
+    navItems.forEach(item => {
+        item.addEventListener('click', function() {
+            navItems.forEach(nav => nav.classList.remove('current'));
+            this.classList.add('current');
+        });
+    });
+
+});
